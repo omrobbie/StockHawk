790f216e518fdb55922d9ecee95cff0f6bb2cfda
omrobbie <putu.roby@yahoo.com>
2017-12-06 08:50:15 +0800

    wip issue #3 detail stock price over time * create detail fragment

--- app/src/main/java/com/udacity/stockhawk/ui/stock_detail/DetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/stock_detail/DetailActivity.java
@@ -1,10 +1,14 @@
 package com.udacity.stockhawk.ui.stock_detail;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.ui.MainActivity;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,15 +18,49 @@ public class DetailActivity extends AppCompatActivity {
     @BindView(R.id.vp_chart)
     ViewPager vp_chart;
 
+    @BindView(R.id.tab_period)
+    TabLayout tab_period;
+
+    private String symbol;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         setupEnv();
+        setupPager();
     }
 
     private void setupEnv() {
         ButterKnife.bind(this);
+
+        symbol = getIntent().getStringExtra(MainActivity.STOCK_SYMBOL);
+        getSupportActionBar().setTitle(symbol);
+    }
+
+    private void setupPager() {
+        DetailAdapter detailAdapter = new DetailAdapter(getSupportFragmentManager());
+
+        detailAdapter.addFragment(setFragment(Calendar.DATE, 7), "Week");
+        detailAdapter.addFragment(setFragment(Calendar.MONTH, 1), "1 month");
+        detailAdapter.addFragment(setFragment(Calendar.MONTH, 3), "3 months");
+
+        tab_period.setupWithViewPager(vp_chart, true);
+        vp_chart.setAdapter(detailAdapter);
+        vp_chart.setOffscreenPageLimit(2);
+    }
+
+    private DetailFragment setFragment(int period, int between) {
+        DetailFragment detailFragment = new DetailFragment();
+        Bundle bundle = new Bundle();
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.add(period, -between);
+        bundle.putLong(DetailFragment.SELECTED_PERIOD, calendar.getTimeInMillis());
+        bundle.putString(DetailFragment.SELECTED_SYMBOL, symbol);
+        detailFragment.setArguments(bundle);
+
+        return detailFragment;
     }
 }
---
+++ app/src/main/java/com/udacity/stockhawk/ui/stock_detail/DetailAdapter.java
@@ -0,0 +1,50 @@
+package com.udacity.stockhawk.ui.stock_detail;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by omrobbie on 06/12/2017.
+ */
+
+public class DetailAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragments = new ArrayList<>();
+    private final List<String> titles = new ArrayList<>();
+
+    public DetailAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position
+     */
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragments.add(fragment);
+        titles.add(title);
+    }
+}
